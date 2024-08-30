ffc3fed15d74f4960fdf633b3ac4dd8f749e56b1
Hiran <dinushahiran@gmail.com>
2024-08-30 20:06:14 +0200

    Added a new class

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -12,6 +12,44 @@ public class Student {
     private int grade;
     private int feesPaid;
     private int feesTotal;
-    private int age;
+
+    /**
+     * To create a new Student by initializing.
+     * Fees for every student is $30,000.
+     * @param id
+     * @param name
+     * @param grade
+     */
+
+    public Student(int id, String name, int grade) {
+        this.feesPaid=0;
+        this.feesTotal=30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+
+    }
+
+    //Not going to alter student's name, student's id.
+
+    /**
+     * used to update the student's name, student's id.
+     * @param grade new grade of the student
+     */
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+
+    /**
+     * keep adding the fees to feesPaid Field;
+     * Add the fees to the fees paid.
+     * The school is going receive the funds
+     *
+     *
+     * @param fees that the student pays
+     */
+    public void updateFeesPaid(int fees){
+        feesPaid += fees;
+    }
 
 }
