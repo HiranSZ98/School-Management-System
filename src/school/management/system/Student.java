929e4a6114ed8dc74744cd85504aaf5c83c1dcea
Hiran <dinushahiran@gmail.com>
2024-08-30 18:37:27 +0200

    first commit

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,16 @@
+package school.management.system;
+
+/**
+ * This class is responsible for keep the
+ * track of students fees, name, grade
+ */
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+}
